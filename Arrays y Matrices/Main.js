64886b0380a23de27adfc1ccd1537166a1e3674f
KevinJp21 <kevinjp821@gmail.com>
2023-07-31 22:11:10 -0500

    .

---
+++ Arrays y Matrices/Main.js
@@ -0,0 +1,35 @@
+let filas, columnas,SumSup=0,SumInf=0;
+
+const matriz = [];
+
+filas=parseInt(prompt('Ingrese el numero de filas'));
+columnas=parseInt(prompt('Ingrese el numero de Columnas'));
+if(filas%2 ===0 & columnas%2 === 0){
+  alert('Ingrese un numero impar');  
+}else{
+    for(let i=0; i < filas; i++){
+        matriz[i] = [];
+        
+        for(let j = 0; j<columnas; j++){
+            matriz[i][j] =  Math.floor(Math.random()*10);
+        }
+    }
+
+    console.log(matriz);
+        
+    
+    for(let i=0; i<filas;i++){
+        for(let j=0;i<columnas;j++){
+            if(j>i){//Se indetermina Suma SUp
+                SumSup = SumSup + matriz[i][j]; 
+            }else if(j<i && j != i){
+                SumInf = SumInf + matriz[i][j]; 
+            }
+        }
+    }
+    
+   console.log('La suma de la diagonal superior es: '+SumSup);
+   console.log('La suma de la diagonal inferior es: '+SumInf);
+}
+
+
